21120eb26ad1fab82c592d36da651e9b5fb8028a
minseo <msp05070478@gmail.com>
2022-09-19 13:55:08 +0900

    FIX : TextActivity

--- Widget and Resource/Design_Widgets/Widgets/app/src/main/java/com/example/widgets/TextActivity.kt
+++ Widget and Resource/Design_Widgets/Widgets/app/src/main/java/com/example/widgets/TextActivity.kt
@@ -2,10 +2,18 @@ package com.example.widgets
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.core.widget.addTextChangedListener
+import com.example.widgets.databinding.ActivityTextBinding
 
 class TextActivity : AppCompatActivity() {
+    val binding by lazy { ActivityTextBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_text)
+
+        binding.editText.addTextChangedListener {
+            Log.d("EditText","현재 입력된 값 = ${it.toString()}")
+        }
     }
 }
