52793c0d3fe1a427e86ba263fb530686782956a7
minseo <msp05070478@gmail.com>
2022-09-21 21:40:49 +0900

    ADD : IntentActivity

---
+++ Screen Composition/Activity/app/src/main/java/com/example/activity/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.activity.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        val intent = Intent(this, SubActivity::class.java)
+        intent.putExtra("from1", "Hello Bundle")
+        intent.putExtra("from2", 2021)
+        binding.btnStart.setOnClickListener { startActivity(intent) }
+    }
+}
---
+++ Screen Composition/Activity/app/src/main/java/com/example/activity/SubActivity.kt
@@ -0,0 +1,16 @@
+package com.example.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.activity.databinding.ActivitySubBinding
+
+class SubActivity : AppCompatActivity() {
+    val binding by lazy { ActivitySubBinding.inflate(layoutInflater) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.to1.text = intent.getStringExtra("from1")
+        binding.to2.text = "${intent.getStringExtra("from2")}"
+    }
+}
